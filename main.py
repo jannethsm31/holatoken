bb9aa316835a33c35dcd3aee3baa671da69f29af
janneth santos <1722110301@utectulancingo.edu.mx>
2023-12-04 15:55:07 +0000

    MMM

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ securityBearer = HTTPBearer()
 @app.get('/')
 def root(credentials: HTTPAuthorizationCredentials = Depends(securityBearer)):
     token = credential.credentials
+    if token = "12treS":
+        return{"auth": True}
+    else
+        return{"auth": False}
     
